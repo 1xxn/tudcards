a5296bf0fa35d3a228791da44ad00490b7ab0adb
1xxn <leon@segebart.de>
2023-07-14 15:25:38 +0200

    Initial Commit

---
+++ tudcards/tudcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  tudcardsApp.swift
+//  tudcards
+//
+//  Created by Leon on 14.07.23.
+//
+
+import SwiftUI
+
+@main
+struct tudcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
